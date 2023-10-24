234cb4050d1b1abd800fa51fed0b6c7c8ec56968
mgermaine93 <mgermaine93@gmail.com>
2023-10-23 22:36:36 -0400

    Organized speaker types and added labels/title to first bar graph.

--- bar_chart_constructor.py
+++ bar_chart_constructor.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import json
 
+# cues up the file
 file = open("transcript_json_2.json")
 
 # this is a list
@@ -13,8 +14,46 @@ data = json.load(file)
 speakers_and_word_counts = {}
 
 for utterance_object in data:
+
     # identify the speaker
-    speaker = utterance_object["speaker"]
+    speaker = ""
+    speaker_label = utterance_object["speaker"]
+
+    # A is Biden (quote)
+    # B is Michelle Martin
+    # C is A Martinez
+    # D is sponsor
+    # E is Zaya Batrawi (reporter)
+    # F is Greg Myri
+    # G is Deirdre Walsh
+    # H is Patrick McHenry (quote)
+    # I is Deepa Fernandez
+    # J is a sponsor
+    # K is Danielle Alarcon
+
+    if speaker_label == "A":
+        speaker = "Interviewees"
+    if speaker_label == "B":
+        speaker = "Hosts"
+    if speaker_label == "C":
+        speaker = "Hosts"
+    if speaker_label == "D":
+        speaker = "Ads"
+    if speaker_label == "E":
+        speaker = "Reporters"
+    if speaker_label == "F":
+        speaker = "Reporters"
+    if speaker_label == "G":
+        speaker = "Reporters"
+    if speaker_label == "H":
+        speaker = "Interviewees"
+    if speaker_label == "I":
+        speaker = "Ads"
+    if speaker_label == "J":
+        speaker = "Ads"
+    if speaker_label == "K":
+        speaker = "Ads"
+    
     text = utterance_object["text"]
 
     if speaker not in speakers_and_word_counts:
@@ -35,30 +74,10 @@ df = pd.DataFrame({
 
 ax = df.plot.barh(
     x = 'labels',
-    y = 'values'
+    y = 'values',
+    title = 'NPR Up First 10/19/2023',
+    xlabel = 'Word Count of Speaker',
+    ylabel = 'Type of Speaker'
 )
 
 plt.show()
-
-# A is Biden (quote)
-# B is Michelle Martin
-# C is A Martinez
-# D is sponsor
-# E is Zaya Batrawi (reporter)
-# F is Greg Myri
-# G is Deirdre Walsh
-# H is Patrick McHenry (quote)
-# I is Deepa Fernandez
-# J is a sponsor
-# K is Danielle Alarcon
-
-
-
-
-# df = pd.DataFrame.from_dict(data)
-
-# # goal: break down the podcast into a bar chart where the independent variable is the speaker and the dependent variable is the word count
-
-
-# speakers = df.groupby("speaker")
-# chart = df.plot.barh(x='speaker',y=)
